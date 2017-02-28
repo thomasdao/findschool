92c91e9c33942d44f9d1bc9c6bd437fae591741d
thomasdao <thomas.daoxuandung@gmail.com>
2017-02-28 12:55:28 +0800

    Change to sqlite3

--- findschool/settings.py
+++ findschool/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'findschool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'dbuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
