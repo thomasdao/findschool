df96213808aedc299b95666bcc9e6e25864d02cc
thomasdao <thomas.daoxuandung@gmail.com>
2017-02-28 12:24:10 +0800

    Update setting

--- findschool/settings.py
+++ findschool/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'findschool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'dbuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
